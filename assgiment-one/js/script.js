c414d911816584bd7eed3505f02b79531ea2af34
Gohar Abbas <163465456+AbbasGohar@users.noreply.github.com>
2024-05-20 12:35:27 +0500

    js assgiment
    its my first js assgiment

---
+++ assgiment-one/js/script.js
@@ -0,0 +1,32 @@
+
+
+// 1.
+var username;
+// 2.
+var myName = "Gohar_Abbas";
+// 3.
+var message = "Hello World";
+alert(message)
+// 4.
+alert("My name is Gohar Abbas");
+alert("I have learn (Html)  (Css) (JavaScript)");
+alert("Now I'm working on (JavaScript)");
+
+// 5.
+var anyname = alert("PIZZA\nPIZZ\nPIZ\nPI\nI");
+
+
+// 6.
+var email = "devgoharabbas@gmail.com";
+alert("Its my gmail " +  email);
+
+// 7.
+var book = " A smatter way to learn JavaScript"
+alert("I am trying to learn from the Book" + book);
+
+// 8.
+document.write("Yah! I can write HTML content through JavaScript");
+
+// 9.
+var sign = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(sign);
